ef3f350ab805929e1e6c6d74faaec5568dc625b9
Arkar Min Ko <arkarminko27@gmail.com>
2025-11-29 22:29:17 +0630

    second commit

---
+++ serverForPOS.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const db = require("./db");
+
+// Routes
+const accounts = require("./POS_routes/accounts")
+
+// CORS helper
+function setCorsHeaders(res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
+const server = http.createServer(async (req, res) => {
+    setCorsHeaders(res);
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
+    const parsedUrl = url.parse(req.url, true);
+    const pathName = parsedUrl.pathname;
+    const method = req.method;
+
+    // Accounts CRUD
+    if (pathName === "/accounts" && method === "POST") accounts.createAccounts(req, res);
+
+    // --- 404 fallback ---
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Route not found" }));
+    }
+})
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
