35f3d9128b0fdca39682fb07d4012a3b00b0aa44
Arkar Min Ko <arkarminko27@gmail.com>
2025-11-29 17:47:15 +0630

    second commit

--- routes/deliverymen.js
+++ routes/deliverymen.js
@@ -4,8 +4,16 @@ const { generatePhotoName } = require("../utils/photoNameGenerator");
 const path = require("path");
 const fs = require("fs");
 
+const UPLOAD_DIR = path.join(__dirname, "../deliverymen_uploads");
+if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR);
+
 function createDeliverymen(req, res) {
-    const form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm({
+        multiples: false,
+        uploadDir: UPLOAD_DIR,
+        keepExtensions: true,
+        encoding: "utf-8",
+    });
     form.uploadDir = path.join(__dirname, "../deliverymen_uploads");
     form.keepExtensions = true;
 
