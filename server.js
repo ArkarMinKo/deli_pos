6628d8e75548f3bd87e66e55a17294e6d40d53a7
Arkar Min Ko <arkarminko27@gmail.com>
2025-11-30 11:59:44 +0630

    second commit

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ const server = http.createServer(async (req, res) => {
     }
 
     // deliveryMen CRUD
-    if (pathName === "/login-deliverymen" && method === "POST") deliverymen.loginDeliverymen(req, res);
+    else if (pathName === "/login-deliverymen" && method === "POST") deliverymen.loginDeliverymen(req, res);
     else if (pathName === "/deliverymen" && method === "POST") deliverymen.createDeliverymen(req, res);
     else if (pathName === "/deliverymen" && method === "GET") deliverymen.getAllDeliverymen(req, res);
 
