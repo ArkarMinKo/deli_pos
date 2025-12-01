fe40690aa6bda4de8cd43aa2c6d78e8c920163ae
Arkar Min Ko <arkarminko27@gmail.com>
2025-12-01 17:09:48 +0630

    new  commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const deliverymen = require("./routes/deliverymen");
 function setCorsHeaders(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "*");
 }
 
 const server = http.createServer(async (req, res) => {
