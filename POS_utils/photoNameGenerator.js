c79f863dd59a2df311c4debc54c7888d94fad556
Arkar Min Ko <arkarminko27@gmail.com>
2026-02-16 21:55:27 +0630

    new  commit

--- POS_utils/photoNameGenerator.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-function generatePhotoName(id, originalName) {
-  const ext = path.extname(originalName);
-  return `${id}${ext}`;
-}
-
-module.exports = { generatePhotoName };
