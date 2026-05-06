62ff6600c99aeccc2cf1a4a1ee08136cea202dbc
Arkar Min Ko <arkarminko27@gmail.com>
2026-05-06 19:11:44 +0630

    new  commit

--- routes/shops.js
+++ routes/shops.js
@@ -423,7 +423,8 @@ function getShopsById(req, res, id) {
         }
 
         if (results.length === 0) {
-            return res.status(404).json({ error: "Account not found" });
+            res.writeHead(400, { "Content-Type": "application/json" });
+            return res.end(JSON.stringify({ error: "Account Not Found" }));
         }
 
         res.writeHead(200, { "Content-Type": "application/json" });
