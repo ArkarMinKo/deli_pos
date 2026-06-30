95137d02cbd5fb3ca6f5cc7dc9efd39f93457ee5
Arkar Min Ko <arkarminko27@gmail.com>
2026-06-30 23:29:06 +0630

    new  commit

--- routes/menu.js
+++ routes/menu.js
@@ -302,7 +302,7 @@ function deleteMenu(req, res, id) {
 
 function getMenuByShopId(req, res, shopId) {
   const shopSql = `
-    SELECT shop_name, shopkeeper_name, email, photo, phone, categories, address, payments, have_deliverymen, deli_fees_method, complete_order, open_shop, location 
+    SELECT shop_name, shopkeeper_name, email, photo, phone, categories, address, payments, have_deliverymen, deli_fees_method, open_shop, location 
     FROM shops WHERE permission = 'approved'
     AND id = ?
   `;
